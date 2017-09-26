d3b00c4b0c82442c74e6656bf832bd337e87ffd0
emilypaige1992 <emilypaigeperry@gmail.com>
2017-09-25 20:36:10 -0400

    updated analysis

--- 3 - Analysis.R
+++ 3 - Analysis.R
@@ -11,24 +11,29 @@ library(MASS)
 source("C:\\Users\\Emily\\Desktop\\DH5\\FHM Research\\1 - Data Manipulation.R")
 
 #Logistic Regression - Currently Use MFP
+analysis2 = analysis[,-2]
 
 
-
-logit1 = glm(use_MFP ~ ., data=analysis, family = "binomial")
+logit1 = glm(use_MFP ~ ., data=analysis2, family = "binomial")
 summary(logit1)
+
+logit1_OR = exp(cbind(OR = coef(logit1),confint(logit1)))
 
 step1 <- stepAIC(logit1, direction="backward", K = 2.7)
 step1$anova
 summary(step1)
+step1_OR = exp(cbind(OR = coef(step1),confint(step1)))
 
 
 #Logistic Regression - Will Ever Use MFP
+analysis3 = analysis[,-1]
 
-logit2 = glm(everuse_MFP ~ ., data=analysis, family = "binomial")
+logit2 = glm(everuse_MFP ~ ., data=analysis3, family = "binomial")
 summary(logit2)
+logit2_OR = exp(cbind(OR = coef(logit2),confint(logit2)))
 
 step2 <- stepAIC(logit2, direction="backward", K = 2.7)
-step2$anova
 summary(step2)
+step2_OR = exp(cbind(OR = coef(step2),confint(step)))
 
 
