9960f04f5bef0d31158e91f56c9ba93039565535
emilypaige1992 <emilypaigeperry@gmail.com>
2017-10-18 20:28:33 -0400

    newest version

--- 3 - Analysis.R
+++ 3 - Analysis.R
@@ -6,18 +6,88 @@ options(scipen = 0)
 #Libraries
 library(ggplot2)
 library(MASS)
-library("glmnet")
-library("nnet")
-
 
 #read in the cleaned data
 source("C:\\Users\\Emily\\Desktop\\DH5\\FHM Research\\1 - Data Manipulation.R")
+analysis$`A3=education_level` = factor(analysis$`A3=education_level`)
+
+#demographics
+#seperate datasets by MFP
+MFP = analysis[analysis$use_MFP == 1,]
+noMFP = analysis[analysis$use_MFP == 0,]
+#TFP Effective
+var = MFP$`C34=FP_effective`
+table(var)
+prop.table(table(var))
+var = noMFP$`C34=FP_effective`
+table(var)
+prop.table(table(var))
+#Age
+var = MFP$AgeBin
+table(var)
+prop.table(table(var))
+var = noMFP$AgeBin
+table(var)
+prop.table(table(var))
+#Cellphone
+var = MFP$`A2=phone`
+table(var)
+prop.table(table(var))
+var = noMFP$`A2=phone`
+table(var)
+prop.table(table(var))
+#Education
+var = MFP$`A3=education_level`
+table(var)
+prop.table(table(var))
+var = noMFP$`A3=education_level`
+table(var)
+prop.table(table(var))
+#Employed
+var = MFP$job
+table(var)
+prop.table(table(var))
+var = noMFP$job
+table(var)
+prop.table(table(var))
+#Live with Partner
+var = MFP$`A7=partner`
+table(var)
+prop.table(table(var))
+var = noMFP$`A7=partner`
+table(var)
+prop.table(table(var))
+#Sexually Active
+var = MFP$`A8=sexually_active`
+table(var)
+prop.table(table(var))
+var = noMFP$`A8=sexually_active`
+table(var)
+prop.table(table(var))
+#Number of Kids
+var = MFP$`A9=children`
+table(var)
+prop.table(table(var))
+var = noMFP$`A9=children`
+table(var)
+prop.table(table(var))
+#More Kids
+var = MFP$`A10=more_kids`
+table(var)
+prop.table(table(var))
+var = noMFP$`A10=more_kids`
+table(var)
+prop.table(table(var))
+#Plan for Kids
+var = MFP$`A12=plan_kids`
+table(var)
+prop.table(table(var))
+var = noMFP$`A12=plan_kids`
+table(var)
+prop.table(table(var))
 
 #Logistic Regression - Currently Use MFP
-myvars <- names(analysis) %in% c("everuse_MFP")
-analysis2 <- analysis[!myvars]
-
-
+analysis2 = analysis[,-2]
 analysis2$`A3=education_level` = factor(analysis2$`A3=education_level`)
 
 logit1 = glm(use_MFP ~ ., data=analysis2, family = "binomial")
@@ -33,9 +103,8 @@ step1_OR = exp(cbind(OR = coef(step1),confint(step1)))
 
 
 #Logistic Regression - Will Ever Use MFP
-
-myvars3 <- names(analysis) %in% c("use_MFP")
-analysis3 <- analysis[!myvars3]
+analysis3 = analysis[,-1]
+analysis3$`A3=education_level` = factor(analysis3$`A3=education_level`)
 
 logit2 = glm(everuse_MFP ~ ., data=analysis3, family = "binomial")
 summary(logit2)
@@ -50,31 +119,3 @@ step2_OR = exp(cbind(OR = coef(step2),confint(step2)))
 #####################################################
 # LASSO
 #####################################################
-
-
-myvars2 <- names(analysis2) %in% c("use_MFP")
-covars <- analysis2[!myvars2]
-
-newc <- covars[-c(1,2,10)]
-
-analysis2$`C34=FP_effective` <- ifelse(analysis2$`C34=FP_effective` == "No" , 0, 1)
-analysis2$`C34=FP_effective`<- factor(analysis2$`C34=FP_effective`, levels = c(0,1), labels = c("No", "Yes"))
-
-h <- factor(analysis2$`C34=FP_effective`)
-
-d <- data.frame(analysis2$`C34=FP_effective`, test)
-
-set.seed(235)
-lassop <- glmnet(x=as.matrix(newc),y=analysis2$"use_MFP",
-                    family = "binomial",alpha=1)
-summary(lassop)
-plot(lassop)
-
-# Lasso
-set.seed(123)
-lassopath <- glmnet(x=as.matrix(covars),y=outcome,
-                    family = "binomial",alpha=1)
-summary(lassopath)
-plot(lassopath, font=2, font.lab=2, main="LASSOpath")
-
-
