a6c66ac63e15c99ffaa45dfcaa653ae2a249aca5
emilypaige1992 <emilypaigeperry@gmail.com>
2017-10-18 20:50:54 -0400

    newest newest

--- 3 - Analysis.R
+++ 3 - Analysis.R
@@ -9,7 +9,6 @@ library(MASS)
 
 #read in the cleaned data
 source("C:\\Users\\Emily\\Desktop\\DH5\\FHM Research\\1 - Data Manipulation.R")
-analysis$`A3=education_level` = factor(analysis$`A3=education_level`)
 
 #demographics
 #seperate datasets by MFP
@@ -94,13 +93,14 @@ logit1 = glm(use_MFP ~ ., data=analysis2, family = "binomial")
 summary(logit1)
 
 logit1_OR = exp(cbind(OR = coef(logit1),confint(logit1)))
+logit1_OR
 
 #reduced
 step1 <- stepAIC(logit1, direction="backward", K = 2.7)
 step1$anova
 summary(step1)
 step1_OR = exp(cbind(OR = coef(step1),confint(step1)))
-
+step1_OR
 
 #Logistic Regression - Will Ever Use MFP
 analysis3 = analysis[,-1]
@@ -109,12 +109,13 @@ analysis3$`A3=education_level` = factor(analysis3$`A3=education_level`)
 logit2 = glm(everuse_MFP ~ ., data=analysis3, family = "binomial")
 summary(logit2)
 logit2_OR = exp(cbind(OR = coef(logit2),confint(logit2)))
+logit2_OR
 
 #reduced
 step2 <- stepAIC(logit2, direction="backward", K = 2.7)
 summary(step2)
 step2_OR = exp(cbind(OR = coef(step2),confint(step2)))
-
+step2_OR
 
 #####################################################
 # LASSO
